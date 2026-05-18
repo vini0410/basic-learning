2ba4c05e10f99b6fb2793977f48c2be1fbe530c0
vini0410 <vini0420@gmail.com>
2026-05-18 19:46:34 -0300

    init notification-app

---
+++ deep-learning/practice-projects/notification-app/src/main/kotlin/com/notification/notificationapp/NotificationAppApplication.kt
@@ -0,0 +1,11 @@
+package com.notification.notificationapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationAppApplication>(*args)
+}
---
+++ deep-learning/practice-projects/notification-app/src/test/kotlin/com/notification/notificationapp/NotificationAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.notification.notificationapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
