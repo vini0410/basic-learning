810b9c1b2fa2588b544439a5127e22d22c0d28da
vini0410 <vini0420@gmail.com>
2026-05-19 17:52:49 -0300

    adding controller for test purposes

---
+++ deep-learning/practice-projects/notification-app/src/main/kotlin/com/notification/notificationapp/integration/income/controller/NotificationController.kt
@@ -0,0 +1,21 @@
+package com.notification.notificationapp.integration.income.controller
+
+import com.notification.notificationapp.core.service.NotificationService
+import com.notification.notificationapp.integration.income.NotificationMessage
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/notify")
+class NotificationController(
+    private val service: NotificationService,
+) {
+
+    @PostMapping
+    fun notify(@RequestBody payload: NotificationMessage): String {
+        service.process(payload.type, payload.metadata, payload.message)
+        return "Sended!!!"
+    }
+}
